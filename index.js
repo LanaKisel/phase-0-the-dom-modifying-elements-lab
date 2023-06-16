2c90018d173538dbb08b3420f6222279548bbc05
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-06-15 17:56:59 -0700

    modifies code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Write your code here!document.createElement('tagName')
+/*document.createElement("div");
+document.body.append("div");
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+  }
+document.body.append(ul);*/
+main.remove(main);
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML="<h1>Lana is the champion!</h1>";
+
