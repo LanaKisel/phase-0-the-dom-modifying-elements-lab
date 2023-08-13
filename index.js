6fd8a45fdf22883b144997be80ee5a585fbad6b8
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-08-12 21:12:52 -0700

    modifying dom elements

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code 
+
+main.remove(main);
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML="<h1>Lana is the champion!</h1>";
+document.appendChild(newHeader)
